45eec52dd4921acbeac47eb366f6b168bea3e86c
TakayukiKiyohara <1>
2018-11-19 22:49:24 +0900

    Chapter_10を追加

---
+++ Chapter_10_00_Answer/Game/Game.cpp
@@ -0,0 +1,94 @@
+#include "stdafx.h"
+#include "Game.h"
+
+
+Game::Game()
+{
+	//カメラを設定。
+	g_camera3D.SetPosition({ 0.0f, 1000.0f, 2200.0f });
+	g_camera3D.SetTarget({ 0.0f, 200.0f, 0.0f });
+	g_camera3D.Update();
+	//Unityちゃんのモデルを初期化。
+	m_unityChanModelDraw.Init(L"Assets/modelData/unityChan.cmo");
+	//背景モデルの初期化処理。
+	m_bgModelDraw.Init(L"Assets/modelData/bg.cmo");
+}
+
+Game::~Game()
+{
+}
+
+
+void Game::Update()
+{
+	//ゲームパッドの更新。
+	for (auto& pad : g_pad) {
+		pad.Update();
+	}
+	m_unityChanPos.x += g_pad[0].GetLStickXF() * -50.0f;
+	m_unityChanPos.z += g_pad[0].GetLStickYF() * -50.0f;
+
+	m_unityChanModelDraw.Update(m_unityChanPos);
+	//シャドウキャスターを登録。
+	m_shadowMap.RegistShadowCaster(&m_unityChanModelDraw);
+	m_shadowMap.RegistShadowCaster(&m_bgModelDraw);
+	//シャドウマップを更新。
+	m_shadowMap.Update(
+		{ 0.0f, 1000.0f, 0.0f },
+		{ 0.0f, 0.0f, 0.0f }
+	);
+}
+
+
+void Game::Render()
+{
+	//描画開始。
+	g_graphicsEngine->BegineRender();
+	///////////////////////////////////////////////
+	//シャドウマップにレンダリング
+	///////////////////////////////////////////////
+	auto d3dDeviceContext = g_graphicsEngine->GetD3DDeviceContext();
+	//現在のレンダリングターゲットをバックアップしておく。
+	ID3D11RenderTargetView* oldRenderTargetView;
+	ID3D11DepthStencilView* oldDepthStencilView;
+	d3dDeviceContext->OMGetRenderTargets(
+		1, 
+		&oldRenderTargetView, 
+		&oldDepthStencilView
+	);
+	//ビューポートもバックアップを取っておく。
+	unsigned int numViewport = 1;
+	D3D11_VIEWPORT oldViewports;
+	d3dDeviceContext->RSGetViewports(&numViewport, &oldViewports);
+
+	//シャドウマップにレンダリング
+	m_shadowMap.RenderToShadowMap();
+
+	//元に戻す。
+	d3dDeviceContext->OMSetRenderTargets(
+		1, 
+		&oldRenderTargetView, 
+		oldDepthStencilView
+	);
+	d3dDeviceContext->RSSetViewports(numViewport, &oldViewports);
+	//レンダリングターゲットとデプスステンシルの参照カウンタを下す。
+	oldRenderTargetView->Release();
+	oldDepthStencilView->Release();
+
+	///////////////////////////////////////////////
+	//ここから通常レンダリング。
+	///////////////////////////////////////////////
+	m_bgModelDraw.Draw(
+		enRenderMode_Normal, 
+		g_camera3D.GetViewMatrix(), 
+		g_camera3D.GetProjectionMatrix()
+	);
+	m_unityChanModelDraw.Draw(
+		enRenderMode_Normal, 
+		g_camera3D.GetViewMatrix(), 
+		g_camera3D.GetProjectionMatrix()
+	);
+
+	//描画終了。
+	g_graphicsEngine->EndRender();
+}
---
+++ Chapter_10_00_Answer/Game/Game.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "C3DModelDraw.h"
+#include "RenderTarget.h"
+#include "ShadowMap.h"
+
+/// <summary>
+/// ゲームクラス。
+/// </summary>
+class Game
+{
+	
+public:
+	/// <summary>
+	/// コンストラクタ。
+	/// </summary>
+	Game();
+	/// <summary>
+	/// デストラクタ。
+	/// </summary>
+	~Game();
+
+	/// <summary>
+	/// 更新処理。
+	/// </summary>
+	void Update();
+	/// <summary>
+	/// 描画処理。
+	/// </summary>
+	void Render();
+private:
+	CVector3 m_unityChanPos = { 0.0f, 0.0f, 0.0f };	//ユニティちゃんの座標。
+	C3DModelDraw m_unityChanModelDraw;		//ユニティちゃんのモデルの描画処理。。
+	C3DModelDraw m_bgModelDraw;				//背景のモデルの描画処理。
+	ShadowMap m_shadowMap;					//シャドウマップ。
+};
+
+
